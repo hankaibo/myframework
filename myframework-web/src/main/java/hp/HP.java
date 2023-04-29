a22ae5cefbda293f1ba062eb18cec6cb91c549f2
BrendanEichDisciple <BrendanEichDisciple@hotmail.com>
2023-04-29 14:43:48 +0800

    temp

---
+++ myframework-web/src/main/java/hp/HP.java
@@ -0,0 +1,43 @@
+package hp;
+
+/**
+ * Created by hanka on 1/14/2017.
+ */
+public class HP {
+    public static void main(String[] args){
+        System.out.println("Hello world.");
+        String a="hp";
+
+        Character b='a';
+
+        boolean aaoe=true;
+        boolean snt=false;
+        int aa=123;
+
+        int c=12;
+
+        float f=123.123f;
+        double d=13.13132D;
+
+        int i1=11;
+        int i2=22;
+        int i3=i1+i2;
+
+        double d1=0.1;
+        double d2=0.2;
+        double d3=d1+d2;
+
+//        System.out.println(i3);
+        System.out.println(d3);
+
+//        int a=1321;
+        int[] arr={1,2,3,4};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+
+
+
+
+    }
+}
